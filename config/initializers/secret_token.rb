7631ab737892f4ebb9c585c8cde2b5ca78347741
nmonje <nicholas.monje@gmail.com>
2011-02-13 15:35:39 -0500

    Initialized.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtfulCommons::Application.config.secret_token = '26963fb4bb73ee196fa2b94453cc8bfce9601b305180a3dee8d2ee0eea0c46ce49c203ce3d23cdb8770a2c690bb72922bf8efbdccfc0fee5deb12e98fc6a0a2e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ArtfulCommons::Application.config.session_store :cookie_store, :key => '_artful_commons_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ArtfulCommons::Application.config.session_store :active_record_store
